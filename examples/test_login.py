b3f008c2becd56ea3e02599b5f4ed2b1516febaa
avinash-br <59632632+avinash-br@users.noreply.github.com>
2023-09-20 22:47:36 +0530

    updated

--- examples/test_login.py
+++ examples/test_login.py
@@ -1,22 +1,41 @@
-from ..login.login import LoginHandle
+from login.login import LoginHandle
 import asyncio
 from fyers_apiv3 import fyersModel
 import toml
 
-with open("../credentials.toml", 'r') as f:
-    config = toml.load(f)
-client_id = config['FYERS']['APP_ID']
 
-bh = LoginHandle()
+class FyersTest:
+    def __init__(self, credentials_file, log_path="../logs"):
+        with open(credentials_file, 'r') as f:
+            self.config = toml.load(f)
+        self.client_id = self.config['FYERS']['APP_ID']
+        self.bh = LoginHandle(credentials_file=credentials_file)
+        self.log_path = log_path
+        self.valid_token = None
+        self.fyers_instance = None
 
-async def get_profile():
-    valid_token = await bh.get_valid_token()
-    fyers = fyersModel.FyersModel(client_id=client_id, is_async=True, 
-                                  token=valid_token, log_path="../logs")
+    async def _initialize_fyers_instance(self):
+        self.valid_token = await self.bh.get_valid_token()
+        self.fyers_instance = fyersModel.FyersModel(client_id=self.client_id,
+                                                    is_async=True, token=self.valid_token,
+                                                    log_path=self.log_path)
 
-    response = await fyers.get_profile()
-    print(response)
-    
+    async def _get_fyers_instance(self):
+        if self.fyers_instance is None:
+            await self._initialize_fyers_instance()
+        return self.fyers_instance
+
+    async def get_profile(self):
+        fyers = await self._get_fyers_instance()
+        response = await fyers.get_profile()
+        return response
+
+    async def get_funds(self):
+        fyers = await self._get_fyers_instance()
+        response = await fyers.funds()
+        return response
+
+
 if __name__ == "__main__":
-    print(__package__)
-    asyncio.run(get_profile())
+    handler = FyersTest(credentials_file="../credentials.toml")
+    asyncio.run(handler.get_funds())
